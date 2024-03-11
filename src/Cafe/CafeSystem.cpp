Luna2026-a11y/cemuF

commit f00cdbc3498c1e0d411f0a5d7c4b393a26863c61

CafeSystem: Init recompiler after game profile has been loaded (#1115)

src/Cafe/CafeSystem.cpp
@@ -779,10 +779,10 @@ namespace CafeSystem
 			return r;
 		// setup memory space and PPC recompiler
         SetupMemorySpace();
-        PPCRecompiler_init();
 		r = SetupExecutable(); // load RPX
 		if (r != STATUS_CODE::SUCCESS)
 			return r;
+		PPCRecompiler_init();
 		InitVirtualMlcStorage();
 		return STATUS_CODE::SUCCESS;
 	}
@@ -821,11 +821,11 @@ namespace CafeSystem
 		uint32 h = generateHashFromRawRPXData(execData->data(), execData->size());
 		sForegroundTitleId = 0xFFFFFFFF00000000ULL | (uint64)h;
 		cemuLog_log(LogType::Force, "Generated placeholder TitleId: {:016x}", sForegroundTitleId);
-		// setup memory space and ppc recompiler
+		// setup memory space
         SetupMemorySpace();
-        PPCRecompiler_init();
         // load executable
         SetupExecutable();
+		PPCRecompiler_init();
 		InitVirtualMlcStorage();
 		return STATUS_CODE::SUCCESS;
 	}

commit 636797d924f72e278600dfef6269b841b81eeb8c

Fix crash introduced by #1115 (#1117)
* Revert "CafeSystem: Init recompiler after game profile has been loaded (#1115)"
* Instead move gameprofile load call

src/Cafe/CafeSystem.cpp
@@ -748,7 +748,6 @@ namespace CafeSystem
 			}
 		}
 		LoadMainExecutable();
-		gameProfile_load();
 		return STATUS_CODE::SUCCESS;
 	}
 
@@ -777,12 +776,13 @@ namespace CafeSystem
 		STATUS_CODE r = LoadAndMountForegroundTitle(titleId);
 		if (r != STATUS_CODE::SUCCESS)
 			return r;
+		gameProfile_load();
 		// setup memory space and PPC recompiler
         SetupMemorySpace();
+        PPCRecompiler_init();
 		r = SetupExecutable(); // load RPX
 		if (r != STATUS_CODE::SUCCESS)
 			return r;
-		PPCRecompiler_init();
 		InitVirtualMlcStorage();
 		return STATUS_CODE::SUCCESS;
 	}
@@ -821,11 +821,11 @@ namespace CafeSystem
 		uint32 h = generateHashFromRawRPXData(execData->data(), execData->size());
 		sForegroundTitleId = 0xFFFFFFFF00000000ULL | (uint64)h;
 		cemuLog_log(LogType::Force, "Generated placeholder TitleId: {:016x}", sForegroundTitleId);
-		// setup memory space
+		// setup memory space and ppc recompiler
         SetupMemorySpace();
+        PPCRecompiler_init();
         // load executable
         SetupExecutable();
-		PPCRecompiler_init();
 		InitVirtualMlcStorage();
 		return STATUS_CODE::SUCCESS;
 	}